b68ae5fe852ccbe6cbfdac0850487db9d521f529
Adam Carter <mail@adam-carter.com>
2018-09-10 11:48:28 -0700

    Abstracted autograde into its own class.  Needs testing. Started working on the CPP autograder and realizing that it will require a bit more setup.  Bummer.

---
+++ autograder.py
@@ -0,0 +1,102 @@
+import os
+import subprocess
+import csv
+import shutil
+from enum import Enum
+
+class OutputLevel(Enum):
+   VERBOSE = 1
+   QUIET = 2 
+
+class Autograder:
+
+   def __init__(self,
+                file_extensions,
+                testing_file,
+                testing_command,
+                testing_options,
+                working_file_name,
+                submission_directory = 'to_test',
+                workspace_directory = 'temp',
+                results_directory = 'results',
+                verbosity = OutputLevel.VERBOSE
+                ):
+      self._file_extensions = file_extensions
+      self._testing_file = testing_file
+      self._testing_command = testing_command
+      self._self._testing_options = testing_options
+      self._working_file_name = working_file_name
+      self._submission_directory = submission_directory
+      self._workspace_directory = workspace_directory
+      self._results_directory = results_directory
+      self._verbosity = verbosity
+      
+   def __get_files(self, extensions):
+
+      if os.path.exists(self._testing_location) == False:
+         os.mkdir(self._testing_location)
+
+      files_to_grade = list()
+      items = os.listdir(self._testing_location)
+      for item in items:
+         item = item.lower()
+         full_path = os.path.join(self._testing_location, item)
+
+         #ignore hidden folders
+         if item[0] == '.':
+               continue
+         
+         #if directory, do a recursive call to find more files
+         if os.path.isdir(item):
+            files_to_grade.append(get_files(full_path, self._extensions))
+         elif:
+            file_name, file_extension = os.path.splitext(item)
+            if file_extension in _extensions:
+               files_to_grade.append(full_path, item))
+      return files_to_grade
+
+   #deletes all files in the specified location
+   def __clear_files(self, location):
+      items = os.listdir(location)
+      for item in items:
+         os.remove(os.path.join(location, item))
+
+   #performs the grading of a single assignment
+   def __grade(self, file_to_grade):
+
+      if os.path.exists(self._temp_directory) == False:
+         os.mkdir(self._temp_directory)
+
+      if self._verbosity == OutputLevel.VERBOSE:
+         print("Testing", file_to_grade)
+
+      #copy over submission file to temporary workspace
+      copy_to_path = os.path.join(self._temp_directory, self._working_file_name)
+      shutil.copyfile(file_to_grade, copy_to_path)
+
+      #copy over testing file to temporary workspace
+      copy_to_path = os.path.join(self._temp_directory, self._testing_file)
+      shutil.copyfile(self._testing_file, copy_to_path)
+
+      #run test, capture results
+      command_path = os.path.join(self._temp_directory, self._testing_command)
+      result = subprocess.run([self._testing_command, self._testing_options, copy_to_path], stdout=subprocess.PIPE, shell=True)
+      result = result.stdout.decode('utf-8').strip()
+      
+      #clear workspace directory
+      self.__clear_files(self._temp_directory)
+
+      #return results
+      return result
+
+   #runs the auto grader
+   def run(self):
+      submissions = self.__get_files()
+      for submission in submissions:
+         result = grade(submission)
+         file_name, file_extension = os.path.splitext(submission)
+         file_name = os.path.split(file_name)
+         file_name = file_name[-1]
+         output = open(os.path.join(self._results_directory, file_name + '.txt'), 'w', encoding="utf-8")
+         print(result, file=output)
+         output.close()
--- autograder_js.py
+++ autograder_js.py
@@ -2,71 +2,25 @@ import os
 import subprocess
 import csv
 import shutil
+from autograder import Autograder
 
 SUBMISSION_DIRECTORY = 'to_test'
 WORKSPACE_DIRECTORY = 'temp'
 RESULTS_DIRECTORY = 'results'
-FILE_EXTENSION = '.html'
+FILE_EXTENSION = list({'.html'})
 TESTING_FILE = 'tester.js'
 TESTING_COMMAND = 'testcafe'
 TESTING_OPTIONS = 'chrome'
 WORKING_FILE_NAME = 'pa1.html'
 
-def get_files(testing_location, extension):
-   files_to_grade = list()
-   items = os.listdir(testing_location)
-   for item in items:
-      item = item.lower()
-      file_name, file_extension = os.path.splitext(item)
-      if file_extension == extension:
-         files_to_grade.append(os.path.join(testing_location, item))
-   return files_to_grade
-
-def clear_files(location):
-   items = os.listdir(location)
-   for item in items:
-      os.remove(os.path.join(location, item))
-
-def grade(file_to_grade, working_file_name, temp_directory, testing_file, testing_command, testing_options):
-
-   print("Testing", file_to_grade)
-
-   #copy over submission file to temporary workspace
-   copy_to_path = os.path.join(temp_directory, working_file_name)
-   shutil.copyfile(file_to_grade, copy_to_path)
-
-   #copy over testing file to temporary workspace
-   copy_to_path = os.path.join(temp_directory, testing_file)
-   shutil.copyfile(testing_file, copy_to_path)
-
-   #run test, capture results
-   command_path = os.path.join(temp_directory, testing_command)
-   result = subprocess.run([testing_command, testing_options, copy_to_path], stdout=subprocess.PIPE, shell=True)
-   result = result.stdout.decode('utf-8').strip()
-   
-   #clear workspace directory
-   clear_files(temp_directory)
-
-   #return results
-   return result
 
 if __name__ == '__main__':
 
-   #set up necessary directories
-   if os.path.exists(SUBMISSION_DIRECTORY) == False:
-      os.mkdir(SUBMISSION_DIRECTORY)
-   if os.path.exists(WORKSPACE_DIRECTORY) == False:
-      os.mkdir(WORKSPACE_DIRECTORY)
-   if os.path.exists(RESULTS_DIRECTORY) == False:
-      os.mkdir(RESULTS_DIRECTORY)
-
-   submissions = get_files(SUBMISSION_DIRECTORY, FILE_EXTENSION)
-   for submission in submissions:
-      result = grade(submission, WORKING_FILE_NAME, WORKSPACE_DIRECTORY, TESTING_FILE, TESTING_COMMAND, TESTING_OPTIONS)
-      file_name, file_extension = os.path.splitext(submission)
-      file_name = os.path.split(file_name)
-      file_name = file_name[-1]
-      output = open(os.path.join(RESULTS_DIRECTORY, file_name + '.txt'), 'w', encoding="utf-8")
-      print(result, file=output)
-      output.close()
+   grader = Autograder(FILE_EXTENSION, 
+                       TESTING_FILE, 
+                       TESTING_COMMAND,
+                       TESTING_OPTIONS, 
+                       WORKING_FILE_NAME
+                       )
+   grader.run()
       
