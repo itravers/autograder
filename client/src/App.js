286739a89f21e9a0d91f8792983f652526e8ddf7
Adam Carter <mail@adam-carter.com>
2018-12-12 08:06:53 -0800

    1. added new npm packages to client and server. 2. added basic db schema. 3. started playing around with front-end UI design.

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,26 @@ class App extends Component {
    render() {
       return (
          <div className="App">
-            
+            <div className="container">
+               <div className="row">
+                  <div className="col">
+                     <ul class="nav nav-tabs">
+                        <li class="nav-item">
+                           <a class="nav-link" href="#">test cases</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link active" href="#">main.cpp</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="#">functions.h</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="#">functions.cpp</a>
+                        </li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
          </div>
       );
    }
