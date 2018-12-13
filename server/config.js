04aeb3ae62d23d63e4a76bb084b40801aaa3b983
Adam Carter <mail@adam-carter.com>
2018-12-12 20:00:42 -0800

    started playing around with a db init script.

--- server/config.js
+++ server/config.js
@@ -1,6 +1,14 @@
 class ServerConstants{
    constructor(){
+      
+      //flag that enables certain features that make local dev easier but may be risky in production
       this.mode = "debug";
+
+      //hash key for generating hashes 
+      this.hash_key = "change this to something secret";
+      this.crypto_method = "sha512";
+
+
       this.data_dir = '../data';
       this.temp_folder = this.data_dir + '/temp';
       this.uploads_folder = this.data_dir + "/uploads";
