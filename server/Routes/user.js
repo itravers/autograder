d772b44cf856b3b999ac7a1125f1935fbed893a2
Alex Childers <abc66@humboldt.edu>
2019-06-01 10:07:03 -0700

    Added helper function for addRoster

--- server/Routes/user.js
+++ server/Routes/user.js
@@ -7,34 +7,66 @@ exports.addRoster = function(req, res, db, acl) {
 
     acl.canModifyCourse(req.session.user, course_id)
        .then(() => {
-          for (let user of roster) {
-             db.Users.exists(user.email)
-               .then(() => {
-                  // select this user's ID 
-                  db.Users.userRow(user.email, (result, err) =>{
-                     // assuming err is null (LATER FIX: HANDLE ERRORS)
-                     user.id = result.id; 
-                     delete user.password; 
-                  })
-               })
-               // if user doesn't exist, create them
-               .catch(() => {
-                  db.Users.create(user, (result, err) => {
-                  
-                  // assuming err is null and result has contents 
-                  // (LATER FIX: HANDLE ERRORS)
-                  user.id = result; 
-                  delete user.password; 
-                  })
-               })
+         for (let user of roster) {
+            convertUser(user)
+            // db.Users.exists(user.email)
+               .then(() => convertUser(user))
                .then(() => db.Courses.addUser(course_id, user.id))
                .then(
                   result => res.json({ response: result })
                )
                .catch(err => {
+                  // throws error here-- what is causing the 
+                  // error and how can we handle it? 
                   res.json({ response: err });
                });
+         }
+      })
+      .catch(err => {
+         res.json({ response: err }); 
+      })
  }
+
+ // adds a user to the database if they don't already exist; 
+ // deletes password from user object and adds their ID from DB
+ exports.convertUser = function(user, db)
+ {
+    // does this user already exist in the database?
+    db.Users.exists(user.email)
+    .then(() =>{
+       // user exists; select their id 
+       db.Users.userRow(user.email, (result, err) => {
+          if(err === null)
+          {
+             user.id = result.id; 
+          }
+          else
+          {
+             // THERE'S GOTTA BE A BETTER WAY TO DO THIS 
+            throw err; 
+          }
+       })
+    })
+    .catch(() => {
+      // user doesn't exist yet, add to database 
+      db.Users.create(user, (result, err) => {
+         if (err === null && result !== null)
+         {
+            user.id = result; 
+         }
+         else
+         {
+            //WTF HOW TO DEAL WITH THIS 
+            throw err; 
+         }
+         user.id = result; 
+      })
+   }); 
+
+    // delete password and return the resulting user 
+    delete user.password;
+    return user; 
+}
  
  // creates new user
  exports.createUser = function(req, res, db) {
@@ -84,4 +116,4 @@ exports.login = function(req, res, db){
 exports.logout = function(req, res) {
     req.session.user = null;
     res.json({ response: req.session.user });
- }
+}
