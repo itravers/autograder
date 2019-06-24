37a5852b1b4f62136417954bcfc65406fd56f055
Alex Childers <abc66@humboldt.edu>
2019-06-24 15:34:57 -0700

    Wrote a test case that interacts with API endpoint directly

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../App';
 import { Provider } from "react-redux";
-import store from "./store/index";
+import store from "../store/index";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
