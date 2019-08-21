1653a6b8e07b088a0da97b1899cd5bf989440a0d
Alex Childers <abc66@humboldt.edu>
2019-08-21 14:09:33 -0700

    Revert "Started test case in which the HTTP request for a user's assignment files fails"
    This reverts commit 13df38aeae0b9372ece6f88c31b10ddfe75d7860.

--- client/src/__tests__/user.test.js
+++ client/src/__tests__/user.test.js
@@ -1,16 +1,11 @@
 describe('create: root_endpoint + "/api/user/create', () => {
-    // server function: createUser()
+
 }); 
 
 describe('login: root_endpoint + "/api/user/login', () => {
-    describe('GET request', () => {
-        // server function: info()
-    });
-    describe('POST request', () => {
-        // server function: login()
-    })
+
 });
 
 describe('logout: root_endpoint + "/api/user/logout', () => {
-    // server function: logout()
+
 }); 
